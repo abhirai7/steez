8cbba67621e0f1165f9f6f4d7e9bd1dd9691f743
rtk-rnjn <ritik0ranjan+github@gmail.com>
2024-09-08 18:41:28 +0530

    feat: Update main.py to use environment variable for port
    This commit modifies the `main.py` file to use an environment variable for the port number instead of hardcoding it. The `PORT` variable is retrieved from the `.env` file using the `dotenv` library. This change allows for greater flexibility and easier configuration of the port number when running the application.

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from __future__ import annotations
+
+import os
+
+from dotenv import load_dotenv
+
 from src.server import app
 
+load_dotenv()
+
+PORT = os.getenv("PORT", 80)
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, threaded=True, host="0.0.0.0")
+    app.run(debug=True, port=int(PORT), threaded=True, host="0.0.0.0")
