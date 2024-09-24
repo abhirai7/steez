7bf403f210817f987b2cc92f9d0be0debb1103a6
abhirai7 <abhirai5031k@gmail.com>
2024-09-24 22:33:55 +0530

    Refactor main.py to use PORT environment variable for app.run()

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ load_dotenv()
 PORT = os.getenv("PORT", 80)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(3000), threaded=True, host="0.0.0.0")
+    app.run(debug=True, port=int(PORT), threaded=True, host="0.0.0.0")
